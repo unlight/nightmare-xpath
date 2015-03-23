70e3cf85fd74d2b0c3bd8dddde6619307cc6ef8f
Roman <s91630277@gmail.com>
2015-03-23 17:26:15 +0300

    Updated example

--- example.js
+++ example.js
@@ -1,10 +1,19 @@
-var dirname = require('path').dirname;
-var phantomjs = require('phantomjs-bin');
-
+var Nightmare = require("nightmare");
+var dirname = require("path").dirname;
+var phantomjs = require("phantomjs-bin");
 var options = {
 	phantomPath: dirname(phantomjs.path) + "/"
 };
+require("./"); // xpath function will be added to prototype.
 
 new Nightmare(options)
-	.goto('http://google.com')
-	.wait()
+	.goto("http://example.com/")
+	.xpath("//a[@href]", function(result) {
+		console.log("Found %d links:", result.length);
+		result.forEach(function(a) {
+			console.log(a.href);
+		});
+	})
+	.run(function(err, nightmare) {
+		if (err) throw err;
+	});
